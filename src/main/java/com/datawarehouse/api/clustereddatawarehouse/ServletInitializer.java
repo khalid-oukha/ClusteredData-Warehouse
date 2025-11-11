2f3f29c0ba2a9e7219d8b61660a988ea50ddb585
khalid-oukha <oukhakhalid@gmail.com>
2025-11-11 21:28:34 +0100

    initialize ClusteredData-Warehouse project with Spring Boot and Maven setup

---
+++ src/main/java/com/datawarehouse/api/clustereddatawarehouse/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.datawarehouse.api.clustereddatawarehouse;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClusteredDataWarehouseApplication.class);
+    }
+
+}
---
+++ src/test/java/com/datawarehouse/api/clustereddatawarehouse/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawarehouse.api.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
